40f17c5f15286a4c245069ef3280b4ecb8df706f
Ebru Arslan <ebru.arslan@eplatform.com.tr>
2020-03-03 11:44:10 +0300

    proje olusturuldu

---
+++ src/main/java/com/example/activeComment/ActiveCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.activeComment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveCommentApplication.class, args);
+	}
+
+}
